9ac901dcdb7480ccbc7d6156214d75a7da925999
Erin Shien <erinshiensmith@gmail.com>
2024-04-28 18:36:26 +0100

    added name input function

--- my-ai-app/src/app/page.js
+++ my-ai-app/src/app/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import styles from "./page.module.css";
 
 import Image from "next/image";
@@ -9,32 +11,63 @@ import { useChat } from "ai/react";
 import { BsChatHeart } from "react-icons/bs";
 
 export default function Chat() {
+  const [userName, setUserName] = useState("");
+  const [nameSubmitted, setNameSubmitted] = useState(false);
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  const handleNameInputChange = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handleNameSubmit = (e) => {
+    e.preventDefault();
+    setUserName(userName.trim());
+    setNameSubmitted(true);
+  };
+
   return (
     <>
-      <Image
-        src="/robot.png"
-        width={500}
-        height={500}
-        alt="Illustration of a robot reading a book, sat on a pile of books"
-      />
+      <div className={styles.imageContainer}>
+        <Image
+          src="/robot.png"
+          width={500}
+          height={500}
+          alt="Illustration of a robot reading a book, sat on a pile of books"
+        />
+      </div>
 
       <div className={`${styles.chatbot} chatbot`}>
-        <form onSubmit={handleSubmit}>
-          <input
-            className="inputField"
-            value={input}
-            placeholder="Ask me anything!"
-            onChange={handleInputChange}
-          />
-          <button onClick={handleSubmit}>
-            <BsChatHeart />
-          </button>
-        </form>
+        {!nameSubmitted && (
+          <form onSubmit={handleNameSubmit}>
+            <input
+              className="inputField"
+              value={userName}
+              placeholder="Enter your name here!"
+              onChange={handleNameInputChange}
+            />
+            <button type="submit">
+              <BsChatHeart />
+            </button>
+          </form>
+        )}
+
+        {nameSubmitted && (
+          <form onSubmit={handleSubmit}>
+            <input
+              className="inputField"
+              value={input}
+              placeholder="Ask me anything!"
+              onChange={handleInputChange}
+            />
+            <button type="submit">
+              <BsChatHeart />
+            </button>
+          </form>
+        )}
 
         {messages.map((m) => (
           <div key={m.id} className={`${styles.chatHistory} chatHistory`}>
-            {m.role === "user" ? "User: " : "AI: "}
+            {m.role === "user" ? `${userName}: ` : "ChatterBot: "}
             {m.content}
           </div>
         ))}
